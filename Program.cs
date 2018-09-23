5bdd8693c0164566b0876e26345c46333f69d051
VitaliiRogozhyn <vitalii.rogozhy@gmail.com>
2018-09-23 17:32:46 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Survey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = tryGetParam();
+            
+
+            Console.WriteLine("What is your age?");
+            var age = tryGetParam();
+                        
+            Console.WriteLine("What month were you born in?");
+            var month = tryGetParam();
+     
+        }
+
+        static string tryGetParam()
+        {
+            var inputParam  = Console.ReadLine();
+            if (inputParam == "")
+            {
+                Console.WriteLine("You didn't type anything please try again"); 
+                tryGetParam();
+            }
+            return inputParam;
+        }
+
+
+        }
+    }
+
