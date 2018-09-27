dacc134a2e0e83c86ef3b14b0f8f704e8166f9dc
Vitalii.Rogozhyn <vitalii.rogozhyn@gmail.com>
2018-09-28 00:59:57 +0300

    Events trying

--- Program.cs
+++ Program.cs
@@ -6,19 +6,39 @@ using System.Threading.Tasks;
 
 namespace Survey
 {
-    class Program
+    class Program 
     {
+        class Data
+        {
+            public string Name;
+            public int Age;
+            public string Month;
+
+            public void Display()
+            {
+                Console.WriteLine($"Your name is {Name}");
+                Console.WriteLine($"Your age is {Age}");
+                Console.WriteLine($"Your birth month is {Month}");
+            }
+        }
+        static public event Action Posted;
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
-            string name = tryGetParam();
+            var stats = new Stats();
+            stats.Start();
+            var data = new Data();
+             Console.WriteLine("What is your name?");
+            data.Name = tryGetParam();
             
 
             Console.WriteLine("What is your age?");
-            var age = tryGetParam();
+            data.Age = int.Parse(tryGetParam());
                         
             Console.WriteLine("What month were you born in?");
-            var month = tryGetParam();
+            data.Month = tryGetParam();
+            if(Posted!=null)
+            Posted();
+            data.Display();
      
         }
 
